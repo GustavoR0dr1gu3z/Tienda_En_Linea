ecce64e5f0d46cd5a19d7a868b99cff28c58b8cf
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-01 15:55:16 -0600

    añadiendo mysql como gestor de BD

--- TiendaEnLineaa/TiendaEnLineaa/settings.py
+++ TiendaEnLineaa/TiendaEnLineaa/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TiendaEnLineaa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TiendaEnLinea',
+        'USER': 'root',
+        'PASSWORD': 'ico',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
